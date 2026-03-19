178943d06c4de59a8d30842dc0111a26c04a6249
Gabriel Silva <dev13.gabriel@gmail.com>
2026-03-19 20:13:56 -0300

    Adicionando Contéudo ao About

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,14 @@
 import Header from "../components/Header/header" 
 import Banner from '../components/Banner/Banner'
 import Footer from '../components/Footer/footer'
+import '../main.css'
 
 function Projects() {
     return ( 
         <>
             <Header />
             <Banner title= "Projects"image="Projects.jpg"/>
+
             <Footer />
 
         </>
